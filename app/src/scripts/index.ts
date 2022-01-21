9adcb2e9b0ecd1af2af7ae03b6740c043acd0bf8
Shahriyor <shahriyordeveloping@gmail.com>
2022-01-21 19:17:35 +0500

    initial

---
+++ app/src/scripts/index.ts
@@ -0,0 +1,20 @@
+const webpUser = document.querySelectorAll('.checkwebp');
+
+
+function support_format_webp(){
+    var elem = document.createElement('canvas');
+    if (!!(elem.getContext && elem.getContext('2d'))){
+        webpUser.forEach(element => {
+            element.classList.add('webp')
+        });
+        return elem.toDataURL('image/webp').indexOf('data:image/webp') == 0;
+    }
+    else{
+        webpUser.forEach(element => {
+            element.classList.add('nowebp')
+        });
+        return false;
+    }
+}
+
+support_format_webp()
