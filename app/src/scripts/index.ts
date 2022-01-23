c3a959c97e7f7539509ad5aaf93a1178f42d0d12
Shahriyor <shahriyordeveloping@gmail.com>
2022-01-23 13:21:23 +0500

    initial

--- app/src/scripts/index.ts
+++ app/src/scripts/index.ts
@@ -16,4 +16,23 @@ function support_format_webp(){
     }
 }
 
+
+function widgetInit(){
+    var options = {
+        whatsapp: "+79041475454", // WhatsApp number
+        telegram: "pottystkr", // Telegram bot username
+        viber: "dixti", // Viber group
+        call: "+79041475454", // Call phone number
+        call_to_action: "Задайте вопрос!", // Call to action
+        button_color: "#FF6550", // Color of button
+        position: "right", // Position may be 'right' or 'left'
+        order: "whatsapp,telegram", // Order of buttons
+    };
+    var proto = document.location.protocol, host = "getbutton.io", url = proto + "//static." + host;
+    var s = document.createElement('script'); s.type = 'text/javascript'; s.async = true; s.src = url + '/widget-send-button/js/init.js';
+    s.onload = function () { WhWidgetSendButton.init(host, proto, options); };
+    var x = document.getElementsByTagName('script')[0]; x.parentNode.insertBefore(s, x);
+}
+
+widgetInit()
 support_format_webp()
